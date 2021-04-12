721a0920111f9354184a4133de840c9f9260f749
Puneeth <puntirock717@gmail.com>
2021-04-13 01:19:21 +0530

    to test mergetool branch two

--- src/main/java/com/pun/poc/PocApplication.java
+++ src/main/java/com/pun/poc/PocApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PocApplication {
 	public static void main(String[] args) {
-
+		//allo?
 		SpringApplication.run(PocApplication.class, args);
-
+		//no i am a branch tw
 		//above comment is removed
 	}
 }
