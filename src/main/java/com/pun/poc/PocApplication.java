8bf4fbf0c3ed20c3cd3231f1e4ba18cbf80f3a06
Puneeth <puntirock717@gmail.com>
2021-04-15 22:39:54 +0530

    Revert "commit con"
    This reverts commit 6228844ca83b54db0158c6b30c4f1ea1d793e2a5.

--- src/main/java/com/pun/poc/PocApplication.java
+++ src/main/java/com/pun/poc/PocApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
-		//This is commit con
-
 		//This is commit dos
 
 	}
